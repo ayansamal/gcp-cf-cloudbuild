005f4d0cecb284a22e90c2c094739275d925a2ec
ayansamal <ayan.samal@simformsolutions.com>
2024-12-05 14:22:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
 from datetime import datetime
-import mysql.connector
+from flask import Flask, request
 
-def return_basic(request):
-  now = datetime.now()
-  return '<h1>Welcome to Cloud Functions and Cloud Build demo by Simform</h1><br/>' + str(now)
+app = Flask(__name__)
+
+@app.route("/")
+def return_basic():
+    now = datetime.now()
+    return '<h1>Welcome to Cloud Functions and Cloud Build demo by Simform</h1><br/>' + str(now)
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))  # Default to port 8080
+    app.run(host="0.0.0.0", port=port)
